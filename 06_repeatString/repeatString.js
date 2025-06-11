a9f213604686c96da6778c16d14aba50771bb117
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-06-11 17:43:06 -0400

    Write solution for exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+  if (num < 0) return "ERROR";
+  let repeatedString = '';
+  for (let i = 0; i < num; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line

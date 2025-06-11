25b71b62db957d5f4282a782276b449656d0b576
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-06-11 17:58:50 -0400

    Write solution for exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+  const splitStr = string.split("");
+  const reversedLetters = splitStr.reverse();
+  return reversedLetters.join("");
 };
 
 // Do not edit below this line

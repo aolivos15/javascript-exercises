5d067f4759972ac9891afb619b07b17ff3cda4a9
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-06-11 18:40:02 -0400

    Write solution for exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...args) => {
+  let filteredArray = [...array];
+  for (let i = 0; i < args.length; i++) {
+    filteredArray = filteredArray.filter(item => item !== args[i]);
+  }
+  return filteredArray;
 };
 
 // Do not edit below this line

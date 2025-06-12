f6ba661ad281869c2f40fd4cab1cc439f97031ca
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-06-12 16:28:06 -0400

    Write solution for exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = (temp) => {
+  const inCelsius = (temp - 32) * (5/9);
+  return Math.round(inCelsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (temp) => {
+  const inFahrenheit = temp * (9/5) + 32;
+  return Math.round(inFahrenheit * 10) / 10
 };
 
 // Do not edit below this line

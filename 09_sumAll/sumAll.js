b8c5ca0c22aa2152b5311b44bce1961e1e0c343a
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-06-12 16:07:11 -0400

    Write solution for exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = (start, end) => {
 
+  if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+
+  let sum = 0;
+
+  for (let i = start; i < end + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

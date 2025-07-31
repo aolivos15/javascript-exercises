d811b8dc7ca7cf069096fa598d5ef02e6cc3720e
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-07-30 20:17:00 -0400

    Write solution for exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num == 0) {
+    return 0;
+  } else if (num < 0) {
+    return 'OOPS';
+  }
+  let sequence = [1, 1];
+  for (let i = 1; i < num; i++) {
+    let nextNum = sequence[i] + sequence[i-1];
+    sequence.push(nextNum);
+  }
+  return sequence[num-1];
 };
 
 // Do not edit below this line

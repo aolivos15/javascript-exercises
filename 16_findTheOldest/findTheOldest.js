d624c5ee6c20fd9f0cc39fd7d6bb3ccf2780b567
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-07-30 20:51:38 -0400

    Write solution for exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // If yearOfDeath is undefined, assign current year
+  people.forEach(person => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+  });
 
+  people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+  return people[0];
 };
 
 // Do not edit below this line

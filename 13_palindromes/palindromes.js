3d8cc8b264537cc46b9e8ee39866e85e1a3c7720
Andrea Olivos <andrea.olivos.b@gmail.com>
2025-07-30 20:04:27 -0400

    Write solution for exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // Remove anything other than letters, convert every letter to lower case
+  let str = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  let reverseStr = str.split("").reverse().join("");
+  if ( str === reverseStr ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
